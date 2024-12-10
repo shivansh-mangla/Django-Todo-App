18a5630cf7d8245e65a62b47dd54c552115fb405
Shivansh <shivanshmangla72@gmail.com>
2024-12-10 16:59:50 +0530

    Added update and delete functionality

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='main-page')
+  path('', views.home, name='main-page'),
+  path('update-task/<int:task_id>/', views.updateTask, name='update_task'),
+  path('delete-task/<int:task_id>/', views.deleteTask, name='delete_task'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
 
 # Create your views here.
@@ -10,16 +10,19 @@ def home(request):
     task = request.POST.get('task')
     new_task = Task(taskName=task)
     new_task.save()
-    print(task)
 
-    tasks = Task.objects.all()  # Retrieve all tasks to display
-    return render(request, 'index.html', {'tasks': tasks})
-
-  return render(request, 'index.html', {})
+  tasks = Task.objects.all()  # Retrieve all tasks to display
+  return render(request, 'index.html', {'tasks': tasks})
 
 
-def deleteTask(request):
-  pass
+def deleteTask(request, task_id):
+  task = get_object_or_404(Task, id=task_id)
+  task.delete()  # Delete the task
+  return redirect('main-page')
 
-def updateTask(request):
-  pass
+
+def updateTask(request, task_id):
+  task = get_object_or_404(Task, id=task_id)
+  task.completed = not task.completed
+  task.save()
+  return redirect('main-page')
