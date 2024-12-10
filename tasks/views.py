6c3491cfcd53ebe2635ca243c6f80e291609f335
Shivansh <shivanshmangla72@gmail.com>
2024-12-10 16:20:56 +0530

    Added basic functions

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from .models import Task
 
 # Create your views here.
 
 
 def home(request):
+  task = ''
+  if request.method == 'POST':
+    task = request.POST.get('task')
+    new_task = Task(taskName=task)
+    new_task.save()
+    print(task)
+
+    tasks = Task.objects.all()  # Retrieve all tasks to display
+    return render(request, 'index.html', {'tasks': tasks})
+
   return render(request, 'index.html', {})
